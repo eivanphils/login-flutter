24820ba4e768be5d57ab1b396f177eb371ec5717
Phils Garcia <eivanphils@gmail.com>
2023-02-13 22:57:30 -0300

    agregado singlechildScrollvoew

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -20,135 +20,137 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: AuthBackground(
-          child: Column(
-        children: [
-          const SizedBox(
-            height: 210,
-          ),
-          CardContainer(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-              child: Column(
-                children: [
-                  Text(
-                    'Registro de nuevo usuario',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-
-                  CustomInputField(
-                    labelText: 'Nombre',
-                    hintText: 'Ingresa tu nombre',
-                    prefixIcon: Icons.person,
-                    onChange: (value) => print,
-                    validator: (value) {
-                      return value!.length < 3 ? 'Por favor ingrese un nombre válido' : null;
-                    }
-                  ),
-
-                  const SizedBox(
-                    height: 15,
-                  ),
-                
-                  CustomInputField(
-                    labelText: 'Apellido',
-                    hintText: 'Ingresa tu apellido',
-                    prefixIcon: Icons.person,
-                    onChange: (value) => print,
-                    validator: (value) {
-                      return value!.length < 3 ? 'Por favor ingrese un apellido válido' : null;
-                    }
-                  ),
-
-                  const SizedBox(
-                    height: 15,
-                  ),
-                
-                  CustomInputField(
-                    prefixIcon: Icons.person,
-                    labelText: 'RUT',
-                    hintText: 'Ingresa tu RUT',
-                    onChange: (value) => print,
-                    validator: (value) {
-                      if (!RutValidator.isValidRut(value!)) {
-                        return 'Por favor ingrese un RUT válido';
-                      }
-                      return null;
-                    },
-                  ),
-
-                  const SizedBox(
-                    height: 15,
-                  ),
-                
-                  CustomInputField(
-                    labelText: 'Correo',
-                    hintText: 'Ingresa tu correo',
-                    prefixIcon: Icons.email,
-                    keyboardType: TextInputType.emailAddress,
-                    onChange: (value) => print,
-                    validator: (value) {
-                      return value!.length < 3 ? 'Por favor ingrese un correo válido' : null;
-                    }
-                  ),
-
-                  const SizedBox(
-                    height: 15,
-                  ),
-                
-                  CustomInputField(
-                    labelText: 'Telefono',
-                    hintText: 'Ingresa tu telefono',
-                    prefixIcon: Icons.phone,
-                    keyboardType: TextInputType.number,
-                    onChange: (value) => print,
-                    validator: (value) {
-                      return value!.length < 3 ? 'Por favor ingrese un telefono válido' : null;
-                    }
-                  ),
-
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  // TODO: crear una clave custom del boton con bordes
-                  _isLoading
-                      ? const CircularProgressIndicator(
-                          color: AppTheme.primaryColor,
-                        )
-                      : ElevatedButton(
-                          onPressed: () async {
-                            FocusScope.of(context).unfocus();
-
-                            _isLoading = true;
-
-                            await Future.delayed(const Duration(seconds: 3));
-
-                            _isLoading = false;
-
-                            Navigator.pop(context, LoginScreen.routeName);
-                          },
-                          child: Container(
-                            width: double.infinity,
-                              padding: const EdgeInsets.symmetric(vertical: 15),
-                              child: Center(child: Text('Crear cuenta'.toUpperCase()))),
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)),
-                              primary: AppTheme.primaryColor,
-                              elevation: 0),
-                        ),
-
-                  const SizedBox(
-                    height: 15,
-                  ),
-                ],
-              ),
+          child: SingleChildScrollView(
+            child: Column(
+                  children: [
+            const SizedBox(
+              height: 210,
             ),
-          )
-        ],
-      )),
+            CardContainer(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                child: Column(
+                  children: [
+                    Text(
+                      'Registro de nuevo usuario',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+          
+                    CustomInputField(
+                      labelText: 'Nombre',
+                      hintText: 'Ingresa tu nombre',
+                      prefixIcon: Icons.person,
+                      onChange: (value) => print,
+                      validator: (value) {
+                        return value!.length < 3 ? 'Por favor ingrese un nombre válido' : null;
+                      }
+                    ),
+          
+                    const SizedBox(
+                      height: 15,
+                    ),
+                  
+                    CustomInputField(
+                      labelText: 'Apellido',
+                      hintText: 'Ingresa tu apellido',
+                      prefixIcon: Icons.person,
+                      onChange: (value) => print,
+                      validator: (value) {
+                        return value!.length < 3 ? 'Por favor ingrese un apellido válido' : null;
+                      }
+                    ),
+          
+                    const SizedBox(
+                      height: 15,
+                    ),
+                  
+                    CustomInputField(
+                      prefixIcon: Icons.person,
+                      labelText: 'RUT',
+                      hintText: 'Ingresa tu RUT',
+                      onChange: (value) => print,
+                      validator: (value) {
+                        if (!RutValidator.isValidRut(value!)) {
+                          return 'Por favor ingrese un RUT válido';
+                        }
+                        return null;
+                      },
+                    ),
+          
+                    const SizedBox(
+                      height: 15,
+                    ),
+                  
+                    CustomInputField(
+                      labelText: 'Correo',
+                      hintText: 'Ingresa tu correo',
+                      prefixIcon: Icons.email,
+                      keyboardType: TextInputType.emailAddress,
+                      onChange: (value) => print,
+                      validator: (value) {
+                        return value!.length < 3 ? 'Por favor ingrese un correo válido' : null;
+                      }
+                    ),
+          
+                    const SizedBox(
+                      height: 15,
+                    ),
+                  
+                    CustomInputField(
+                      labelText: 'Telefono',
+                      hintText: 'Ingresa tu telefono',
+                      prefixIcon: Icons.phone,
+                      keyboardType: TextInputType.number,
+                      onChange: (value) => print,
+                      validator: (value) {
+                        return value!.length < 3 ? 'Por favor ingrese un telefono válido' : null;
+                      }
+                    ),
+          
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    // TODO: crear una clave custom del boton con bordes
+                    _isLoading
+                        ? const CircularProgressIndicator(
+                            color: AppTheme.primaryColor,
+                          )
+                        : ElevatedButton(
+                            onPressed: () async {
+                              FocusScope.of(context).unfocus();
+          
+                              _isLoading = true;
+          
+                              await Future.delayed(const Duration(seconds: 3));
+          
+                              _isLoading = false;
+          
+                              Navigator.pop(context, LoginScreen.routeName);
+                            },
+                            child: Container(
+                              width: double.infinity,
+                                padding: const EdgeInsets.symmetric(vertical: 15),
+                                child: Center(child: Text('Crear cuenta'.toUpperCase()))),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)),
+                                primary: AppTheme.primaryColor,
+                                elevation: 0),
+                          ),
+          
+                    const SizedBox(
+                      height: 15,
+                    ),
+                  ],
+                ),
+              ),
+            )
+                  ],
+                ),
+          )),
     );
   }
 }
