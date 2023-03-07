866832d8d5374a4ac51b4b162b6f4679833e46fb
Phils Garcia <eivanphils@gmail.com>
2023-03-07 12:56:00 -0300

    Agregada opacidad en la imagen del producto

--- lib/widgets/product_image.dart
+++ lib/widgets/product_image.dart
@@ -10,28 +10,32 @@ class ProductImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    print(imageUrl);
+
     return Container(
       width: double.infinity,
       height: size.height * 0.55,
       decoration: _boxDecoration(),
-      child: ClipRRect(
-        borderRadius: _bottomBorderRadius(),
-        child: FadeInImage(
-          image: NetworkImage(imageUrl.toString()),
-          placeholder: const AssetImage('assets/loading.gif'),
-          imageErrorBuilder: (BuildContext context, Object obj, stackTrace) =>
-              const Image(
-            image: AssetImage('assets/no-image.jpg'),
+      child: Opacity(
+        opacity: 0.85,
+        child: ClipRRect(
+          borderRadius: _bottomBorderRadius(),
+          child: FadeInImage(
+            image: NetworkImage(imageUrl.toString()),
+            placeholder: const AssetImage('assets/loading.gif'),
+            imageErrorBuilder: (BuildContext context, Object obj, stackTrace) =>
+                const Image(
+              image: AssetImage('assets/no-image.jpg'),
+              fit: BoxFit.cover,
+            ),
             fit: BoxFit.cover,
           ),
-          fit: BoxFit.cover,
         ),
       ),
     );
   }
 
   BoxDecoration _boxDecoration() => BoxDecoration(
+    color: Colors.black,
       boxShadow: [AppTheme.boxShadow()], borderRadius: _bottomBorderRadius());
 
   BorderRadius _bottomBorderRadius() {
