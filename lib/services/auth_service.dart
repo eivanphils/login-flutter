e9371eccdc18973a9d0182c40506f99d938f79b6
Phils Garcia <eivanphils@gmail.com>
2023-05-01 15:35:58 -0400

    Se guarda el idToken del usuario logueado

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,11 +2,14 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
   final String _baseUrl = 'identitytoolkit.googleapis.com';
   final String _apiKey = 'AIzaSyDWBmKpajLT0t9WJmZgylaaevAHw2q0p0I';
+
+  final FlutterSecureStorage storage = const FlutterSecureStorage();
 
   Future<String?> createUser(String email, String password) async {
     // create Map to send body data
@@ -22,12 +25,13 @@ class AuthService extends ChangeNotifier {
 
     print(decodedResponde);
     if (decodedResponde.containsKey('idToken')) {
-      // guardar el token en el securedStorage
+      storage.write(key: 'idToken', value: decodedResponde['idToken']);
       return null;
     } else {
       return decodedResponde['error']['message'];
     }
   }
+
   Future<String?> loginUser(String email, String password) async {
     // create Map to send body data
     final Map<String, dynamic> authData = {
@@ -35,17 +39,22 @@ class AuthService extends ChangeNotifier {
       'password': password
     };
 
-    final url = Uri.https(_baseUrl, '/v1/accounts:signInWithPassword', {'key': _apiKey});
+    final url = Uri.https(
+        _baseUrl, '/v1/accounts:signInWithPassword', {'key': _apiKey});
 
     final response = await http.post(url, body: json.encode(authData));
     final Map<String, dynamic> decodedResponde = json.decode(response.body);
 
     print(decodedResponde);
     if (decodedResponde.containsKey('idToken')) {
-      // guardar el token en el securedStorage
+      await storage.write(key: 'idToken', value: decodedResponde['idToken']);
       return null;
     } else {
       return decodedResponde['error']['message'];
     }
+  }
+
+  Future logout() async {
+    await storage.delete(key: 'idToken');
   }
 }
