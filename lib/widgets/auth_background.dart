377c4506fac4968e8aa404f4562a94c157cc2516
Phils Garcia <eivanphils@gmail.com>
2023-10-12 21:14:36 -0300

    Agregada configuracion para ejecutar en macOS

--- lib/widgets/auth_background.dart
+++ lib/widgets/auth_background.dart
@@ -4,7 +4,7 @@ import 'package:login_flutter/theme/app_theme.dart';
 class AuthBackground extends StatelessWidget {
   final Widget child;
 
-  const AuthBackground({Key? key, required this.child}) : super(key: key);
+  const   AuthBackground({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import file_selector_macos
+import flutter_secure_storage_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+}
